e0bab50b7f17764065c47d733b07931bdc0d206e
dioppolo <diego.ioppolo2006@gmail.com>
2026-06-08 11:11:52 +0200

    ex0 finito

--- ex0/construct.py
+++ ex0/construct.py
@@ -9,20 +9,28 @@ def main():
     print("Prefix: ", sys.prefix)
     print("Base prefix: ", sys.base_prefix)
     if sys.prefix != sys.base_prefix:
-        print("\nVirtual envirorment detected!")
+        print(
+            "\nVirtual envirorment detected!\n"
+            "\nMATRIX STATUS: Welcome to the construct\n"
+            "\nSUCCESS: you're in an isolated enviroment!\n"
+            "Safe to install packages without affecting ", end=""
+            "the global system\n"
+            "\nPackage installation path:\n"
+            )
+        for path in site.getsitepackages():
+            print(path)
     else:
         print("\nNo virtual envirorment detected")
-        print("=== Instruction ===")
+        print("\nMATRIX STATUS: you're still plugged in")
+        print("\nWARNING: You're in the global environment!")
+        print("The machine cansee everything you install")
+        print("\n=== Instruction ===")
         print("Create VE with:")
         print("python3 -m venv 'Nome_Dir'")
         print("\nActivate it:")
         print("Linux/macOS: source Nome_Dir/bin/activate")
-        print("Windows: 'Nome_File'\\scripts\\activate")
-    print("\nCurrent site-packages:")
-    for path in site.getsitepackages():
-        print(path)
-    print("\nUser site-packages:")
-    print(site.getusersitepackages())
+        print("Windows: Nome_Dir\\scripts\\activate")
+        print("\nThen run this program again")
 
 
 if __name__ == "__main__":
